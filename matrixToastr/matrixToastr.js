efa1c06e80b80058ea7610890da55869adc9b272
sysuKinthon <qinkai1994@qq.com>
2017-03-20 18:22:42 +0800

    删除了无用代码

--- matrixToastr/matrixToastr.js
+++ matrixToastr/matrixToastr.js
@@ -283,26 +283,7 @@ $(function()
           showDuration: 300,
           showEasing: 'swing', //swing and linear are built into jQuery
           onShown: undefined,
-          hideMethod: 'fadeOut',
-          hideDuration: 1000,
-          hideEasing: 'swing',
-          onHidden: undefined,
-          closeMethod: false,
-          closeDuration: false,
-          closeEasing: false,
-          closeOnHover: true,
 
-          extendedTimeOut: 1000,
-          iconClasses: {
-              error: 'toast-error',
-              info: 'toast-info',
-              success: 'toast-success',
-              warning: 'toast-warning',
-              confirm: 'toast-confirm'
-          },
-          iconClass: 'toast-info',
-          positionClass: 'toast-top-right',
-          timeOut: 5000, // Set timeOut and extendedTimeOut to 0 to make it sticky
           titleClass: 'toast-title',
           messageClass: 'toast-message',
           escapeHtml: false,
@@ -310,9 +291,6 @@ $(function()
           closeHtml: '<button type="button">&times;</button>',
           closeClass: 'toast-close-button',
           newestOnTop: true,
-          preventDuplicates: false,
-          progressBar: false,
-          progressClass: 'toast-progress',
           rtl: false
       };
   }
